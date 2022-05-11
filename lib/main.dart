ab6292bc2631e14d87e3dd77005b5f60ef87ff43
alisflutter <alismonapara354@gmail.com>
2022-05-11 18:09:07 +0530

    fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+// import 'dart:ffi';
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +15,53 @@ class MyApp extends StatefulWidget {
 }
 
 class _songFile extends State<MyApp> {
+  late Permission
+      permission; //                                        //  request for permission
+  PermissionStatus permissionStatus = PermissionStatus.denied;
+
+  void initState() {
+    _listionfor();
+    super.initState();
+    // _request();
+  }
+
+  Future<void> _requestfor() async {
+    final status = await Permission.storage.request();
+    setState(() {
+      permissionStatus = status;
+    });
+  }
+
+  void _listionfor() async {
+    final status = await Permission.storage.status;
+
+    setState(() {
+      permissionStatus = status;
+    });
+
+    switch (status) {
+      case PermissionStatus.denied:
+        _requestfor();
+        break;
+      case PermissionStatus.granted:
+        break;
+      case PermissionStatus.restricted:
+        Navigator.pop(context);
+        break;
+      case PermissionStatus.limited:
+        break;
+      case PermissionStatus.permanentlyDenied:
+        Navigator.pop(context);
+        break;
+      default:
+        break;
+    }
+  }
+
+  // void _request() {
+  //   Permission.storage.request();
+  // }
+
   final audio = new OnAudioQuery();
   @override
   Widget build(BuildContext context) {
@@ -19,15 +70,37 @@ class _songFile extends State<MyApp> {
         appBar: AppBar(
           title: Text('Music'),
         ),
-        body: ListView.builder(
-          itemBuilder: (context, index) => ListTile(
-            leading: Icon(Icons.music_note_rounded),
-            title: Text("music$index"),
-            subtitle: Text('arist'),
-            trailing: Icon(Icons.more_horiz),
-            onTap: () {},
+        body: FutureBuilder<List<SongModel>>(
+          future: audio.querySongs(
+            sortType: null,
+            orderType: OrderType.ASC_OR_SMALLER,
+            uriType: UriType.EXTERNAL,
+            ignoreCase: true,
           ),
-          itemCount: 50,
+          builder: (context, item) {
+            if (item.data == null) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (item.data!.isEmpty) {
+              return Center(
+                  // child: Text('No song found'),
+                  // child: CircularProgressIndicator(),
+                  );
+            }
+
+            return ListView.builder(
+              itemBuilder: (context, index) => ListTile(
+                leading: Icon(Icons.music_note_rounded),
+                title: Text(item.data![index].displayNameWOExt),
+                subtitle: Text('${item.data![index].artist}'),
+                trailing: Icon(Icons.more_horiz),
+                onTap: () {},
+              ),
+              itemCount: item.data!.length,
+            );
+          },
         ),
       ),
     );
